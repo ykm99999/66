03552e88332ae9af0f87969b22ae68cb16ab23a1
ykm99999 <119298674+ykm99999@users.noreply.github.com>
2026-03-14 21:28:42 +0800

    Update platform.mk

--- 888/platform.mk
+++ 888/platform.mk
@@ -1,1 +1,82 @@
+#
+# Copyright (c) 2023, MediaTek Inc. All rights reserved.
+# Copyright (c) 2026, ykm888 (Physical Hardening for SL-3000)
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
 
+MTK_PLAT		:=	plat/mediatek
+MTK_PLAT_SOC		:=	$(MTK_PLAT)/$(PLAT)
+APSOC_COMMON		:=	$(MTK_PLAT)/apsoc_common
+
+# 启用 Cortex-A53 勘误修复
+ERRATA_A53_826319	:=	1
+ERRATA_A53_836870	:=	1
+ERRATA_A53_855873	:=	1
+
+PROGRAMMABLE_RESET_ADDRESS	:=	1
+ENABLE_SVE_FOR_NS	:=	0
+MULTI_CONSOLE_API	:=	1
+RESET_TO_BL2		:=	1
+
+# FIP 对齐 (8字节)
+FIP_ARGS		+=	--align 8
+
+PLAT_INCLUDES		:=	-I$(APSOC_COMMON)				\
+				-I$(APSOC_COMMON)/drivers/uart			\
+				-I$(APSOC_COMMON)/drivers/trng/v2		\
+				-I$(APSOC_COMMON)/drivers/wdt			\
+				-Iinclude/plat/arm/common			\
+				-Iinclude/plat/arm/common/aarch64		\
+				-I$(MTK_PLAT_SOC)/drivers/spmc			\
+				-I$(MTK_PLAT_SOC)/drivers/timer			\
+				-I$(MTK_PLAT_SOC)/drivers/pll			\
+				-I$(MTK_PLAT_SOC)/drivers/devapc		\
+				-I$(MTK_PLAT_SOC)/include			\
+				-I$(MTK_PLAT_SOC)/drivers/dram
+
+# 包含子模块
+include $(MTK_PLAT_SOC)/bl2pl/bl2pl.mk
+include $(MTK_PLAT_SOC)/bl2/bl2.mk
+include $(MTK_PLAT_SOC)/bl31/bl31.mk
+include $(MTK_PLAT_SOC)/drivers/efuse/efuse.mk
+
+# --- 【物理加固：强行注入驱动源】 ---
+ifeq ($(BOOT_DEVICE),nor)
+BL2_SOURCES		+=	$(MTK_PLAT_SOC)/bl2/bl2_dev_spi_nor.c
+endif
+
+include $(APSOC_COMMON)/bl2/tbbr_post.mk
+include $(APSOC_COMMON)/bl2/ar_post.mk
+include $(APSOC_COMMON)/bl2/bl2_image_post.mk
+
+# 安全分区大小定义
+OPTEE_TZRAM_SIZE := 0x10000
+ifneq ($(BL32),)
+ifeq ($(TRUSTED_BOARD_BOOT),1)
+DEFINES += -DNEED_BL32
+OPTEE_TZRAM_SIZE := 0x500000
+endif
+endif
+DEFINES += -DOPTEE_TZRAM_SIZE=$(OPTEE_TZRAM_SIZE)
+
+# 依赖规则包含
+include make_helpers/dep.mk
+
+# --- 【物理加固：逻辑注册】 ---
+# 确保 bl2_dev_spi_nor 在依赖链中，使其被正确编译
+$(call GEN_DEP_RULES,bl2,bl2_dev_spi_nor emicfg dram_log bl2_boot_ram bl2_boot_nand_nmbm bl2_dev_mmc bl2_plat_init bl2_plat_setup mt7981_gpio dtb)
+$(call MAKE_DEP,bl2,bl2_dev_spi_nor,BOOT_DEVICE)
+
+$(call MAKE_DEP,bl2,emicfg,DRAM_USE_DDR4 DRAM_SIZE_LIMIT DDR3_FREQ_2133 DDR3_FREQ_1866 BOARD_QFN BOARD_BGA)
+$(call MAKE_DEP,bl2,dram_log,DRAM_DEBUG_LOG)
+$(call MAKE_DEP,bl2,bl2_plat_init,BL2_COMPRESS)
+$(call MAKE_DEP,bl2,bl2_plat_setup,BOOT_DEVICE TRUSTED_BOARD_BOOT DUAL_FIP)
+$(call MAKE_DEP,bl2,bl2_dev_mmc,BOOT_DEVICE)
+$(call MAKE_DEP,bl2,bl2_boot_ram,RAM_BOOT_DEBUGGER_HOOK RAM_BOOT_UART_DL)
+$(call MAKE_DEP,bl2,bl2_boot_nand_nmbm,NMBM_MAX_RATIO NMBM_MAX_RESERVED_BLOCKS NMBM_DEFAULT_LOG_LEVEL)
+$(call MAKE_DEP,bl2,mt7981_gpio,ENABLE_JTAG)
+$(call MAKE_DEP,bl2,dtb,BOOT_DEVICE)
+
+$(call GEN_DEP_RULES,bl31,bl31_plat_setup)
+$(call MAKE_DEP,bl31,bl31_plat_setup,ANTI_ROLLBACK TRUSTED_BOARD_BOOT)
