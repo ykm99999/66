bcaf431687f75a3562ede058d285e1da4630e5c1
ykm99999 <119298674+ykm99999@users.noreply.github.com>
2026-03-20 09:32:04 +0800

    Update mt7981_sl3000.mk

--- 888/mt7981_sl3000.mk
+++ 888/mt7981_sl3000.mk
@@ -1,5 +1,5 @@
 #
-# SL3000 (司络) 设备定义 - 1GB 正常版
+# SL3000 设备定义 - 1GB 正常版
 # 硬件：MT7981B + 1GB DDR4 + 128G eMMC + 32MB SPI NOR
 #
 
@@ -7,7 +7,7 @@ define Device/sl_3000-emmc
   DEVICE_VENDOR := SL
   DEVICE_MODEL := 3000 eMMC (1GB)
   DEVICE_DTS := mt7981-sl-3000-emmc
-  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  DEVICE_DTS_DIR := $(DTS_DIR)
   SUPPORTED_DEVICES := sl,3000-emmc
   DEVICE_PACKAGES := \
     kmod-usb3 kmod-usb-storage kmod-usb-storage-uas \
@@ -83,27 +83,5 @@ define Device/sl_3000-emmc
   IMAGES := sysupgrade.bin
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
-TARGET_DEVICES += sl_3000-emmc
 
-# -----------------------------------------------------------------------------
-# 编程器救砖镜像（可选，用于 SPI NOR 编程器烧录）
-# 布局（偏移量基于 U-Boot 配置）：
-#   0x000000 - 0x1FFFFF : BL2 (填充至2M)
-#   0x200000 - 0x3FFFFF : FIP (U-Boot)
-#   0x400000 - 0xBFFFFF : 内核 (8M)
-#   0xC00000 - 0x1FFFFFF : 根文件系统 (20M)
-#
-# 若要启用，请取消注释以下块，并确保已生成对应的 BL2/FIP 文件：
-#   - mt7981-nor-ddr4-bl2-1g.img   (1GB 版 BL2)
-#   - mt7981_sl3000_nor-fip.bin    (U-Boot FIP)
-#
-# define Device/sl_3000-emmc-programmer
-#   $(Device/sl_3000-emmc)
-#   IMAGES += nor-programmer-dump.bin
-#   IMAGE/nor-programmer-dump.bin := \
-#	append-image-stage mt7981-nor-ddr4-bl2-1g.img | pad-to 2048k | \
-#	append-image-stage mt7981_sl3000_nor-fip.bin | pad-to 4096k | \
-#	append-kernel | pad-to 12M | \
-#	append-rootfs | pad-to 32M | check-size 32M
-# endef
-# TARGET_DEVICES += sl_3000-emmc-programmer
+TARGET_DEVICES += sl_3000-emmc
