52253dff11a21d6310edf9e28944a4225f67572b
ykm99999 <119298674+ykm99999@users.noreply.github.com>
2026-04-01 07:51:22 +0800

    Update mt7981_sl3000.mk

--- 888/mt7981_sl3000.mk
+++ 888/mt7981_sl3000.mk
@@ -1,8 +1,3 @@
-#
-# SL3000 设备定义 - 1GB 正常版
-# 硬件：MT7981B + 1GB DDR4 + 128G eMMC + 32MB SPI NOR
-#
-
 define Device/sl_3000-emmc
   DEVICE_VENDOR := SL
   DEVICE_MODEL := 3000 eMMC (1GB)
@@ -12,13 +7,7 @@ define Device/sl_3000-emmc
   DEVICE_PACKAGES := \
     kmod-usb3 kmod-usb-storage kmod-usb-storage-uas \
     f2fsck losetup mkf2fs kmod-fs-f2fs kmod-mmc \
-    luci-app-ksmbd luci-i18n-ksmbd-zh-cn ksmbd-utils \
-    luci-app-passwall2 \
-    xray-core chinadns-ng \
-    shadowsocks-libev-ss-local shadowsocks-libev-ss-redir shadowsocks-libev-ss-tunnel \
-    shadowsocks-rust-sslocal simple-obfs \
-    docker-ce docker-compose kmod-br-netfilter kmod-ikconfig kmod-ipt-physdev \
-    kmod-nf-ipt6 kmod-nf-ipvs kmod-veth kmod-fs-overlay luci-app-dockerman
+    luci-app-ksmbd luci-i18n-ksmbd-zh-cn ksmbd-utils
   IMAGES := sysupgrade.bin
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
