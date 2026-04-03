1317a2ea24b45bd02cf4ba500b1e90520534116e
ykm99999 <119298674+ykm99999@users.noreply.github.com>
2026-04-03 19:58:19 +0800

    Update mt7981_sl3000.mk

--- 888/mt7981_sl3000.mk
+++ 888/mt7981_sl3000.mk
@@ -1,21 +1,4 @@
-# SL3000 eMMC 设备定义（精简版，无科学上网、Docker）
-define Device/sl_3000-emmc
-  DEVICE_VENDOR := SL
-  DEVICE_MODEL := 3000 eMMC (1GB)
-  DEVICE_DTS := mt7981-sl-3000-emmc
-  DEVICE_DTS_DIR := $(DTS_DIR)
-  SUPPORTED_DEVICES := sl,3000-emmc
-  DEVICE_PACKAGES := \
-    kmod-usb3 kmod-usb-storage kmod-usb-storage-uas \
-    f2fsck losetup mkf2fs kmod-fs-f2fs kmod-mmc \
-    luci-app-ksmbd luci-i18n-ksmbd-zh-cn ksmbd-utils
-  IMAGES := sysupgrade.bin
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
-endef
-
-TARGET_DEVICES += sl_3000-emmc
-
-# SL3000 SPI-NOR 救砖设备定义（不变）
+# SL3000 SPI-NOR 救砖设备定义
 define Device/sl_3000-spi-nor
   DEVICE_VENDOR := SL
   DEVICE_MODEL := 3000 SPI-NOR (32MB)
