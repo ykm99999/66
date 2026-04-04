e605315af73606c3cce80b83513ed40bc25a84f2
ykm99999 <119298674+ykm99999@users.noreply.github.com>
2026-04-04 16:00:19 +0800

    Update mt7981_sl3000.mk

--- 888/mt7981_sl3000.mk
+++ 888/mt7981_sl3000.mk
@@ -1,25 +1,23 @@
-define Device/sl_3000-spi-nor
+define Device/mt7981_sl3000_emmc
   DEVICE_VENDOR := SL
-  DEVICE_MODEL := 3000 SPI-NOR (32MB Flash)
-  DEVICE_DTS := mt7981-sl-3000-emmc
-  DEVICE_DTS_DIR := $(DTS_DIR)
-  SUPPORTED_DEVICES := sl,3000-spi-nor
-  # 救砖全家桶（含你之前要的 spi、mmc、分区、uboot工具）
+  DEVICE_MODEL := SL3000
+  DEVICE_VARIANT := eMMC
+  DEVICE_DTS := mt7981b-sl3000-emmc
+  SUPPORTED_DEVICES := sl,sl3000
+
+  KERNEL_LOADADDR := 0x48000000
+
+  # eMMC 救砖完整包（适配你机器）
   DEVICE_PACKAGES := \
-    block-mount \
-    uboot-envtools \
-    ttyd \
-    mmc-utils \
-    spi-utils \
-    mtd-utils \
-    kmod-mtd-rw
+	luci luci-base luci-mod-system luci-theme-bootstrap \
+	block-mount e2fsprogs f2fs-tools \
+	kmod-fs-ext4 kmod-fs-f2fs \
+	kmod-mtd kmod-mtd-rw \
+	kmod-sdhci-mtk \
+	dropbear \
+	lsblk blkid mount-utils \
+	mtd-utils uboot-envtools
 
-  # 关键：只生成完整编程器固件 .bin，不生成 sysupgrade
-  IMAGES := factory.bin
-  IMAGE_SIZE := 32768k
-
-  # 完整一体固件（直接用于编程器烧录 SPI NOR 32MB）
-  IMAGE/factory.bin := append-kernel | append-rootfs | pad-rootfs | pad-to 32768k
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
-
-TARGET_DEVICES += sl_3000-spi-nor
+TARGET_DEVICES += mt7981_sl3000_emmc
