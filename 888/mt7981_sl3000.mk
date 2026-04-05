a998a731397f860d58ba637e02d86dd085f010d5
ykm99999 <119298674+ykm99999@users.noreply.github.com>
2026-04-05 22:06:46 +0800

    Update mt7981_sl3000.mk

--- 888/mt7981_sl3000.mk
+++ 888/mt7981_sl3000.mk
@@ -1,4 +1,4 @@
-# SL3000 eMMC 完整版设备定义
+# SL3000 eMMC 完整版设备定义（含科学上网、Docker）
 define Device/mt7981_sl3000_emmc
   DEVICE_VENDOR := SL
   DEVICE_MODEL := SL3000
@@ -15,9 +15,31 @@ define Device/mt7981_sl3000_emmc
     dropbear \
     lsblk blkid mount-utils \
     mtd-utils uboot-envtools \
-    kmod-mt7981-eth kmod-mt7531   # 添加有线网络驱动
+    kmod-mt7981-eth kmod-mt7531 \
+    luci-app-passwall2 xray-core chinadns-ng \
+    shadowsocks-libev-ss-local shadowsocks-libev-ss-redir shadowsocks-libev-ss-tunnel \
+    shadowsocks-rust-sslocal simple-obfs \
+    docker-ce docker-compose kmod-br-netfilter kmod-ikconfig kmod-ipt-physdev \
+    kmod-nf-ipt6 kmod-nf-ipvs kmod-veth kmod-fs-overlay luci-app-dockerman
   IMAGES := sysupgrade.bin
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 
 TARGET_DEVICES += mt7981_sl3000_emmc
+
+# SL3000 SPI-NOR 救砖设备定义（精简，不含科学上网、Docker）
+define Device/sl_3000-spi-nor
+  DEVICE_VENDOR := SL
+  DEVICE_MODEL := 3000 SPI-NOR (32MB)
+  DEVICE_DTS := mt7981b-sl3000-emmc
+  DEVICE_DTS_DIR := $(DTS_DIR)
+  SUPPORTED_DEVICES := sl,3000-spi-nor
+  DEVICE_PACKAGES := \
+    block-mount \
+    uboot-envtools
+  IMAGES := sysupgrade.bin
+  IMAGE_SIZE := 25600k
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+
+TARGET_DEVICES += sl_3000-spi-nor
