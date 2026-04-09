97fdb49a57bc66a9275376da71f0f0dc59252062
ykm99999 <119298674+ykm99999@users.noreply.github.com>
2026-04-09 21:53:46 +0800

    Update mt7981_sl3000.mk

--- 888/mt7981_sl3000.mk
+++ 888/mt7981_sl3000.mk
@@ -1,45 +1,34 @@
-# SL3000 eMMC 完整版设备定义（含科学上网、Docker）
-define Device/mt7981_sl3000_emmc
-  DEVICE_VENDOR := SL
+include ./common/common.mk
+
+define Device/mt7981_sl3000_spi_rescue
+  DEVICE_VENDOR := Siluo
   DEVICE_MODEL := SL3000
-  DEVICE_VARIANT := eMMC
-  DEVICE_DTS := mt7981b-sl3000-emmc
-  SUPPORTED_DEVICES := sl,sl3000
-  KERNEL_LOADADDR := 0x48000000
+  DEVICE_VARIANT := 1GB-DDR4-32MB-SPI-Rescue
+  DEVICE_DTS := mt7981b-sl3000-spi-nor
+  SUPPORTED_DEVICES := siluo,sl3000-spi-nor
+
+  SOC := mt7981
+  UBOOTENV_IN_FLASH := 1
+  KERNEL_IN_UBI := 0
+
+  # 修正内核加载地址（二选一，建议 0x40800000）
+  KERNEL_LOADADDR := 0x40800000
+  # 或者 0x48000000
+
+  # 生成标准救砖镜像
+  IMAGES := rescue.bin
+  IMAGE/rescue.bin := append-kernel | pad-to 6M | append-rootfs | pad-rootfs
+
+  # 确保 initramfs 内核被正确包含
+  ifneq ($(CONFIG_TARGET_ROOTFS_INITRAMFS),)
+    IMAGE/rescue.bin := append-image-stage initramfs-kernel.bin | pad-to 6M
+  endif
+
   DEVICE_PACKAGES := \
-    luci luci-base luci-mod-system luci-theme-bootstrap \
-    block-mount e2fsprogs f2fs-tools \
-    kmod-fs-ext4 kmod-fs-f2fs \
-    kmod-mtd kmod-mtd-rw \
-    kmod-mmc-mtk \
-    dropbear \
-    lsblk blkid mount-utils \
-    mtd-utils uboot-envtools \
-    kmod-mt7981-eth kmod-mt7531 \
-    luci-app-passwall2 xray-core chinadns-ng \
-    shadowsocks-libev-ss-local shadowsocks-libev-ss-redir shadowsocks-libev-ss-tunnel \
-    shadowsocks-rust-sslocal simple-obfs \
-    docker-ce docker-compose kmod-br-netfilter kmod-ikconfig kmod-ipt-physdev \
-    kmod-nf-ipt6 kmod-nf-ipvs kmod-veth kmod-fs-overlay luci-app-dockerman
-  IMAGES := sysupgrade.bin
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+    uboot-envtools mtd-utils kmod-mtd-rw \
+    block-mount kmod-mmc kmod-mmc-mtk \
+    kmod-fs-ext4 kmod-fs-f2fs f2fs-tools e2fsprogs \
+    ip-full dropbear
 endef
 
-TARGET_DEVICES += mt7981_sl3000_emmc
-
-# SL3000 SPI-NOR 救砖设备定义（精简，不含科学上网、Docker）
-define Device/sl_3000-spi-nor
-  DEVICE_VENDOR := SL
-  DEVICE_MODEL := 3000 SPI-NOR (32MB)
-  DEVICE_DTS := mt7981b-sl3000-emmc
-  DEVICE_DTS_DIR := $(DTS_DIR)
-  SUPPORTED_DEVICES := sl,3000-spi-nor
-  DEVICE_PACKAGES := \
-    block-mount \
-    uboot-envtools
-  IMAGES := sysupgrade.bin
-  IMAGE_SIZE := 25600k
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
-endef
-
-TARGET_DEVICES += sl_3000-spi-nor
+TARGET_DEVICES += mt7981_sl3000_spi_rescue
