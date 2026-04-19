61337fa6145903b0b06a096921a0723186ed372b
ykm99999 <119298674+ykm99999@users.noreply.github.com>
2026-04-19 19:18:41 +0800

    Update filogic.mk

--- immortalwrt/target/linux/mediatek/image/filogic.mk
+++ immortalwrt/target/linux/mediatek/image/filogic.mk
@@ -1,4 +1,10 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# SL3000 专属精简版 filogic.mk
+# 基于 ImmortalWrt/OpenWrt 原版提取必要宏，仅保留 mt7981_sl3000_spi_rescue 设备
+
 DTS_DIR := $(DTS_DIR)/mediatek
+
+# ========== 通用构建宏（从原版提取，必需） ==========
 
 define Image/Prepare
 	# For UBI we want only one extra block
@@ -109,179 +115,34 @@ define Build/cetron-header
 	rm $@.tmp
 endef
 
-define Device/abt_asr3000
-  DEVICE_VENDOR := ABT
-  DEVICE_MODEL := ASR3000
-  DEVICE_DTS := mt7981b-abt-asr3000
-  DEVICE_DTS_DIR := ../dts
-  DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware
-  UBINIZE_OPTS := -E 5
-  BLOCKSIZE := 128k
-  PAGESIZE := 2048
-  KERNEL_IN_UBI := 1
-  UBOOTENV_IN_UBI := 1
-  IMAGES := sysupgrade.itb
-  KERNEL_INITRAMFS_SUFFIX := -recovery.itb
+# ========== SL3000 救砖设备定义 ==========
+
+define Device/mt7981_sl3000_spi_rescue
+  DEVICE_VENDOR := Siluo
+  DEVICE_MODEL := SL3000
+  DEVICE_VARIANT := SPI Full (1GB DDR4 / 32MB NOR)
+  DEVICE_DTS := mt7981b-sl3000-emmc
+  SUPPORTED_DEVICES := siluo,sl3000-emmc
+  DEVICE_DRAM_SIZE := 1024M
+  SOC := mt7981
+  KERNEL_LOADADDR := 0x44000000
   KERNEL := kernel-bin | gzip
-  KERNEL_INITRAMFS := kernel-bin | lzma | \
-        fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
-  IMAGE/sysupgrade.itb := append-kernel | \
-        fit gzip $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb external-static-with-rootfs | append-metadata
-  ARTIFACTS := preloader.bin bl31-uboot.fip
-  ARTIFACT/preloader.bin := mt7981-bl2 spim-nand-ddr3
-  ARTIFACT/bl31-uboot.fip := mt7981-bl31-uboot abt_asr3000
-endef
-TARGET_DEVICES += abt_asr3000
-
-define Device/acelink_ew-7886cax
-  DEVICE_VENDOR := Acelink
-  DEVICE_MODEL := EW-7886CAX
-  DEVICE_DTS := mt7986a-acelink-ew-7886cax
-  DEVICE_DTS_DIR := ../dts
-  DEVICE_PACKAGES := kmod-mt7915e kmod-mt7986-firmware mt7986-wo-firmware
-  UBINIZE_OPTS := -E 5
-  BLOCKSIZE := 128k
-  PAGESIZE := 2048
-  IMAGE_SIZE := 65536k
-  KERNEL_IN_UBI := 1
-  IMAGES += factory.bin
-  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
-endef
-TARGET_DEVICES += acelink_ew-7886cax
-
-define Device/acer_predator-w6
-  DEVICE_VENDOR := Acer
-  DEVICE_MODEL := Predator Connect W6
-  DEVICE_DTS := mt7986a-acer-predator-w6
-  DEVICE_DTS_DIR := ../dts
-  DEVICE_DTS_LOADADDR := 0x47000000
-  DEVICE_PACKAGES := kmod-usb3 kmod-mt7915e kmod-mt7986-firmware kmod-mt7916-firmware \
-	mt7986-wo-firmware f2fsck mkf2fs automount
-  IMAGES := sysupgrade.bin
-  KERNEL := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
-  KERNEL_INITRAMFS := kernel-bin | lzma | \
-	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
-endef
-TARGET_DEVICES += acer_predator-w6
-
-define Device/acer_predator-w6d
-  DEVICE_VENDOR := Acer
-  DEVICE_MODEL := Predator Connect W6d
-  DEVICE_DTS := mt7986a-acer-predator-w6d
-  DEVICE_DTS_DIR := ../dts
-  DEVICE_DTS_LOADADDR := 0x47000000
-  DEVICE_PACKAGES := kmod-usb3 kmod-mt7915e kmod-mt7916-firmware kmod-mt7986-firmware mt7986-wo-firmware e2fsprogs f2fsck mkf2fs
-  IMAGES := sysupgrade.bin
-  KERNEL := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
-  KERNEL_INITRAMFS := kernel-bin | lzma | \
-	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
-endef
-TARGET_DEVICES += acer_predator-w6d
-
-define Device/acer_vero-w6m
-  DEVICE_VENDOR := Acer
-  DEVICE_MODEL := Connect Vero W6m
-  DEVICE_DTS := mt7986a-acer-vero-w6m
-  DEVICE_DTS_DIR := ../dts
-  DEVICE_DTS_LOADADDR := 0x47000000
-  DEVICE_PACKAGES := kmod-leds-ktd202x kmod-mt7915e kmod-mt7916-firmware kmod-mt7986-firmware mt7986-wo-firmware e2fsprogs f2fsck mkf2fs
-  IMAGES := sysupgrade.bin
-  KERNEL := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
-  KERNEL_INITRAMFS := kernel-bin | lzma | \
-	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
-endef
-TARGET_DEVICES += acer_vero-w6m
-
-define Device/adtran_smartrg
-  DEVICE_VENDOR := Adtran
-  DEVICE_DTS_DIR := ../dts
-  DEVICE_PACKAGES := e2fsprogs f2fsck mkf2fs kmod-hwmon-pwmfan
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  IMAGES := spi-full-32mb.bin
+  IMAGE/spi-full-32mb.bin := gen_spi_full_32mb
+  DEVICE_PACKAGES := uboot-envtools mtd-utils kmod-mtd-rw block-mount kmod-mmc kmod-mmc-mtk kmod-fs-ext4 kmod-fs-f2fs f2fs-tools e2fsprogs ip-full dropbear
 endef
 
-define Device/smartrg_sdg-8612
-$(call Device/adtran_smartrg)
-  DEVICE_MODEL := SDG-8612
-  DEVICE_DTS := mt7986a-smartrg-SDG-8612
-  DEVICE_PACKAGES += kmod-mt7915e kmod-mt7986-firmware mt7986-wo-firmware
+define Build/gen_spi_full_32mb
+	@echo "=== 物理对齐检查：开始合成 32MB 救砖固件 ==="
+	dd if=/dev/zero bs=1M count=32 | tr '\000' '\377' > $@.tmp
+	# 偏移对齐：0M->BL2, 1M->FIP, 2M->Factory, 3M->Kernel
+	[ -f $(BIN_DIR)/bl2.img ] && dd if=$(BIN_DIR)/bl2.img of=$@.tmp conv=notrunc status=none
+	[ -f $(BIN_DIR)/fip.bin ] && dd if=$(BIN_DIR)/fip.bin of=$@.tmp bs=1M seek=1 conv=notrunc status=none
+	[ -f $(BIN_DIR)/factory.bin ] && dd if=$(BIN_DIR)/factory.bin of=$@.tmp bs=1M seek=2 conv=notrunc status=none
+	# 名称对齐点：引用 mt7981b-sl3000-emmc 编译生成的 itb 文件
+	[ -f $(KDIR)/fit-mt7981b-sl3000-emmc.itb ] && dd if=$(KDIR)/fit-mt7981b-sl3000-emmc.itb of=$@.tmp bs=1M seek=3 conv=notrunc status=none
+	mv $@.tmp $@
+	@echo "✅ 物理链路闭合：已使用 mt7981b-sl3000-emmc 完成合成"
 endef
-TARGET_DEVICES += smartrg_sdg-8612
 
-define Device/smartrg_sdg-8614
-$(call Device/adtran_smartrg)
-  DEVICE_MODEL := SDG-8614
-  DEVICE_DTS := mt7986a-smartrg-SDG-8614
-  DEVICE_PACKAGES += kmod-mt7915e kmod-mt7986-firmware mt7986-wo-firmware
-endef
-TARGET_DEVICES += smartrg_sdg-8614
-
-define Device/smartrg_sdg-8622
-$(call Device/adtran_smartrg)
-  DEVICE_MODEL := SDG-8622
-  DEVICE_DTS := mt7986a-smartrg-SDG-8622
-  DEVICE_PACKAGES += kmod-mt7915e kmod-mt7915-firmware kmod-mt7986-firmware mt7986-wo-firmware
-endef
-TARGET_DEVICES += smartrg_sdg-8622
-
-define Device/smartrg_sdg-8632
-$(call Device/adtran_smartrg)
-  DEVICE_MODEL := SDG-8632
-  DEVICE_DTS := mt7986a-smartrg-SDG-8632
-  DEVICE_PACKAGES += kmod-mt7915e kmod-mt7915-firmware kmod-mt7986-firmware mt7986-wo-firmware
-endef
-TARGET_DEVICES += smartrg_sdg-8632
-
-define Device/smartrg_sdg-8733
-$(call Device/adtran_smartrg)
-  DEVICE_MODEL := SDG-8733
-  DEVICE_DTS := mt7988a-smartrg-SDG-8733
-  DEVICE_PACKAGES += kmod-mt7996-firmware kmod-phy-aquantia kmod-usb3 mt7988-wo-firmware
-endef
-TARGET_DEVICES += smartrg_sdg-8733
-
-define Device/smartrg_sdg-8733a
-$(call Device/adtran_smartrg)
-  DEVICE_MODEL := SDG-8733A
-  DEVICE_DTS := mt7988d-smartrg-SDG-8733A
-  DEVICE_PACKAGES += mt7988-2p5g-phy-firmware kmod-mt7996-233-firmware kmod-phy-aquantia mt7988-wo-firmware
-endef
-TARGET_DEVICES += smartrg_sdg-8733a
-
-define Device/smartrg_sdg-8734
-$(call Device/adtran_smartrg)
-  DEVICE_MODEL := SDG-8734
-  DEVICE_DTS := mt7988a-smartrg-SDG-8734
-  DEVICE_PACKAGES += kmod-mt7996-firmware kmod-phy-aquantia kmod-sfp kmod-usb3 mt7988-wo-firmware
-endef
-TARGET_DEVICES += smartrg_sdg-8734
-
-define Device/asus_rt-ax52
-  DEVICE_VENDOR := ASUS
-  DEVICE_MODEL := RT-AX52
-  DEVICE_DTS := mt7981b-asus-rt-ax52
-  DEVICE_DTS_DIR := ../dts
-  DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware
-  IMAGES := sysupgrade.bin
-  KERNEL := kernel-bin | lzma | \
-	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
-  KERNEL_INITRAMFS := kernel-bin | lzma | \
-	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
-ifeq ($(IB),)
-  ARTIFACTS := initramfs.trx
-  ARTIFACT/initramfs.trx := append-image-stage initramfs-kernel.bin | \
-	uImage none | asus-trx -v 3 -n $$(DEVICE_MODEL)
-endif
-endef
-TARGET_DEVICES += asus_rt-ax52
-
-define Device/asus_rt-ax59u
-  DEVICE_VENDOR := ASUS
-  DEVICE_MODEL := RT-AX59U
-  DEVICE_DTS := mt7986a-asus-rt-ax59u
-  DEVICE_DTS_DIR := ../dts
-  DEVICE_PACKAGE
+TARGET_DEVICES += mt7981_sl3000_spi_rescue
